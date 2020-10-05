4b08522cbcecd361f405e7e0aa090bb53edb15a6
Mw3y <maxence1604.minecraft@gmail.com>
2020-10-05 10:39:47 +0200

    Format

--- js/home.js
+++ js/home.js
@@ -31,25 +31,51 @@ powered by GSAP : https://www.greensock.com/
 
 var fireflies = document.getElementById("fireflies");
 
-var total=20,
-    container=fireflies,
-    w=fireflies.offsetWidth,
-    h=fireflies.offsetHeight,
-    Tweens=[],
-    SPs=1;
+var total = 20,
+  container = fireflies,
+  w = fireflies.offsetWidth,
+  h = fireflies.offsetHeight,
+  Tweens = [],
+  SPs = 1;
 
-for (var i=total;i--;){ 
-	var Div=document.createElement('div');
-	TweenLite.set(Div,{attr:{class:'dot'},x:R(w),y:R(h),opacity:0});
-	container.appendChild(Div);	Anim(Div);	Tweens.push(Div);
+for (var i = total; i--;) {
+  var Div = document.createElement('div');
+  TweenLite.set(Div, {
+    attr: {
+      class: 'dot'
+    },
+    x: R(w),
+    y: R(h),
+    opacity: 0
+  });
+  container.appendChild(Div);
+  Anim(Div);
+  Tweens.push(Div);
 };
 
-function Anim(elm){ 
-	elm.Tween=TweenLite.to(elm,R(20)+25,{bezier:{values:[{x:R(w),y:R(h)},{x:R(w),y:R(h)}]},opacity:R(1.5),scale:R(1.5)+0.5,delay:R(2),onComplete:Anim,onCompleteParams:[elm]})
+function Anim(elm) {
+  elm.Tween = TweenLite.to(elm, R(20) + 25, {
+    bezier: {
+      values: [{
+        x: R(w),
+        y: R(h)
+      }, {
+        x: R(w),
+        y: R(h)
+      }]
+    },
+    opacity: R(1.5),
+    scale: R(1.5) + 0.5,
+    delay: R(2),
+    onComplete: Anim,
+    onCompleteParams: [elm]
+  })
 };
 
-for(var i=total;i--;){
-  Tweens[i].Tween.play()};
+for (var i = total; i--;) {
+  Tweens[i].Tween.play()
+};
 
-function R(max){return Math.random()*max};
-
+function R(max) {
+  return Math.random() * max
+};
